b434f9e88f3a0783d1bcb080599b03078c11d015
mburman <mburman@andrew.cmu.edu>
2013-10-20 13:17:07 -0400

    Starting a local server now to avoid this cross domain stuff. Will update the readme with instructions on how to get everything to work

---
+++ run.sh
@@ -0,0 +1,14 @@
+LOG_FILE="serverlog"
+PORT=8000
+
+echo "Starting local server...."
+
+python -m SimpleHTTPServer $PORT &> $LOG_FILE &
+
+if [ $? -eq 0 ];
+then
+  echo "Started server on port $PORT"
+  echo "View the timeline at http://localhost:8000/timeline.html"
+else
+  echo "Failed to start server. Something else is using port $PORT. Kill it and try again."
+fi
